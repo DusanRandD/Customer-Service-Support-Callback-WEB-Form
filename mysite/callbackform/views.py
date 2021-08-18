0841fd42872185a8f1dab178efc6b56b25f81ebe
DusanRandD <58935261+DusanRandD@users.noreply.github.com>
2021-08-18 11:03:52 +0200

    Update views.py

--- mysite/callbackform/views.py
+++ mysite/callbackform/views.py
@@ -6,8 +6,10 @@ from .models import CallbackForm
 # Create your views here.
 
 def display_callback_form(request):
+    # if user enter information in form
     if request.method=='POST':
         form = CallbackFormForm(request.POST)
+        # if entered values are good pass it to database
         if form.is_valid():
             CallbackForm.objects.create(name=request.POST['name'],
                                        phone_number=request.POST['phone_number'],
@@ -17,9 +19,12 @@ def display_callback_form(request):
                                        problem_description=request.POST['problem_description'],
                                        support_datetime=request.POST['support_datetime']
                                        )
+            # Send user confirmation
             return render(request, 'callbackform/confirmation.html', {})
+        # If enteries are bad go back to form page that user continue to edit
         else:
             return render(request, 'callbackform/contact_form.html', {'form': form})
+    # If user first time load page sent empty form
     else:
         form = CallbackFormForm()
         return render(request,'callbackform/contact_form.html',{'form':form})
